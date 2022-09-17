6c4ea3bac270b00f23bfc29d3334ab0c8781bae3
Damien Burke <damien@damienburke.com>
2022-09-16 17:15:28 -0700

    Extract a template for cop pages

--- _plugins/los_angeles_police.rb
+++ _plugins/los_angeles_police.rb
@@ -20,13 +20,21 @@ module LosAngelesPolice
   class CopPage < Jekyll::PageWithoutAFile
     def initialize(site, cop)
       @data = cop
-      @content = 'a cop page for <{{ page }}>'
+      @content = page_template
 
       super(site, __dir__, '', filename)
     end
 
     def filename
       "us/ca/police/los_angeles/#{data['SerialNo']}.html"
+    end
+
+    def page_template
+      @page_template ||= File.read(page_template_path)
+    end
+
+    def page_template_path
+      File.expand_path('./los_angeles_police/cop.html', __dir__)
     end
   end
 end
