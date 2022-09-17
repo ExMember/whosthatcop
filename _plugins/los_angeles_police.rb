b06828dc748ba96e613641fbadf8ff90d2bcec6e
Damien Burke <damien@damienburke.com>
2022-09-16 17:00:59 -0700

    Extract CopPage#filename method

--- _plugins/los_angeles_police.rb
+++ _plugins/los_angeles_police.rb
@@ -12,10 +12,15 @@ module LosAngelesPolice
   end
 
   class CopPage < Jekyll::PageWithoutAFile
-     def initialize(site, cop)
-      super(site, __dir__, '', "us/ca/police/los_angeles/#{cop['SerialNo']}.html")
+    def initialize(site, cop)
       @data = cop
       @content = 'a cop page for <{{ page }}>'
+
+      super(site, __dir__, '', filename)
+    end
+
+    def filename
+      "us/ca/police/los_angeles/#{data['SerialNo']}.html"
     end
   end
 end
